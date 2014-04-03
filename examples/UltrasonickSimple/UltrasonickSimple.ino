e14370c49a05eb0e8a28926bdf878e4dcd5c3b30
EricK Simões <erick.simoes@yahoo.com.br>
2014-04-03 02:39:29 -0300

    feat: Example file
    Insert an example file.
    Update color map variables with new names.

---
+++ examples/UltrasonickSimple/UltrasonickSimple.ino
@@ -0,0 +1,30 @@
+/*
+  UltrassonickSimple
+  Print in the Serial Monitor the distance read for the HR-SC04 Ultrasonic Sensor in centimeters.
+
+  The circuit:
+  * Module HR-SC04 attached to digital pins as follows:
+  ** Trig - pin 12
+  ** Echo - pin 13
+
+  created in April 3, 2014
+  by EricK Simoes (@AloErickSimoes)
+
+  http://ericksimoes.com.br/ultrasonick/
+
+  This example code is in the public domain.
+ */
+
+#include <Ultrasonick.h>
+
+Ultrasonick ultrasonick(12, 13);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print("Distance in CM:");
+  Serial.println(ultrasonick.distanceRead()); // By default the distance is returned in centimeters
+  delay(1000);
+}
