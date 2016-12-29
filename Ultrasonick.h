a2a3bd019aaa89dcc7a4f4a1f5cbc0f05e0a6c70
Erick Simões <erick.simoes@cesar.org.br>
2016-12-29 13:20:56 -0300

    docs: Definition of improvements to be made

--- Ultrasonick.h
+++ Ultrasonick.h
@@ -1,3 +1,13 @@
+/**
+* TODO: Change license to BSD-3-Clause
+*/
+
+/**
+* TODO: Update layout of folders and files follows the new
+* specification of libraries.
+* https://github.com/arduino/Arduino/wiki/Arduino-IDE-1.5:-Library-specification
+*/
+
 /*
   Ultrasonick.h - Library for HC-SR04 Ultrasonic Ranging Module in a minimalist way.
   Created by EricK Simoes (@AloErickSimoes), April 3, 2014.
@@ -16,6 +26,11 @@
 #define CM 1
 #define INC 0
 
+/**
+* TODO: Consider discontinuing use of classes and using structures
+* TODO: Remove the underscore of the private variables name
+* and use static keyword
+*/
 class Ultrasonick {
   public:
     Ultrasonick(int trigPin, int echoPin);
