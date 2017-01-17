93079e78ad3c9a3f9c3beb9d94c4ba54a7bd9718
Erick Simões <erick.simoes@live.com>
2017-01-17 17:10:52 -0300

    style: Remove the underscore of the private variables name
    Resolves: #5

--- src/Ultrasonick.cpp
+++ src/Ultrasonick.cpp
@@ -16,19 +16,19 @@
 #include "Ultrasonick.h"
 
 Ultrasonick::Ultrasonick(uint8_t trigPin, uint8_t echoPin) {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  _trigPin = trigPin;
-  _echoPin = echoPin;
+  trig = trigPin;
+  echo = echoPin;
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
 }
 
 int Ultrasonick::timing() {
-  digitalWrite(_trigPin, LOW);
+  digitalWrite(trig, LOW);
   delayMicroseconds(2);
-  digitalWrite(_trigPin, HIGH);
+  digitalWrite(trig, HIGH);
   delayMicroseconds(10);
-  digitalWrite(_trigPin, LOW);
-  return pulseIn(_echoPin, HIGH); // duration
+  digitalWrite(trig, LOW);
+  return pulseIn(echo, HIGH); // duration
 }
 
 /**
