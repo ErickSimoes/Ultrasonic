5148c93b6475d3defcce815e0a5946a44e70479e
Erick Simões <erick.simoes@live.com>
2017-01-23 19:40:12 -0300

    Update documentation (#26)
    * docs: Update documentation in source files
    
    Resolves: #22
    
    * docs: Update the sample file
    
    Resolves: #20
    
    * feat: Sound velocity constants are define
    
    The speed of sound is no longer a magic number.
    It is now easier to change the value from a define.
    
    Resolves: #7
    
    * feat: Extra files to documentation
    
    Include picture and Fritzing of connections.
    
    * docs: Update README.md
    
    Resolves: #25

--- src/Ultrasonick.cpp
+++ src/Ultrasonick.cpp
@@ -1,11 +1,21 @@
 /*
-  Ultrasonick.h - Library for HC-SR04 Ultrasonic Ranging Module in a minimalist way.
-  Created by EricK Simoes (@AloErickSimoes), April 3, 2014.
-  Released into the Creative Commons Attribution-ShareAlike 4.0 International.
-*/
+ * Ultrasonick.h
+ * 
+ * Library for HC-SR04 Ultrasonic Ranging Module in a minimalist way
+ *
+ * created 3 Apr 2014
+ * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
+ * modified 23 Jan 2017
+ * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
+ * 
+ * Released into the MIT License.
+ */
 
 #include <Arduino.h>
 #include "Ultrasonick.h"
+
+#define CM_DIVISOR  28
+#define INC_DIVISOR 71
 
 Ultrasonick::Ultrasonick(uint8_t trigPin, uint8_t echoPin) {
   trig = trigPin;
@@ -23,21 +33,18 @@ int Ultrasonick::timing() {
   return pulseIn(echo, HIGH); // duration
 }
 
-/**
-* TODO: Enable the change of sound velocity constants
-*/
 int Ultrasonick::distanceRead(uint8_t und) {
   if (und)
-    return timing() / 28 / 2 ; //distance in CM
+    return timing() / CM_DIVISOR / 2;  //distance in CM
   else
-    return timing() / 71 / 2; //distance in INC
+    return timing() / INC_DIVISOR / 2; //distance in INC
 }
 
 int Ultrasonick::distanceRead() {
   /*
-  If the unit of measure is not passed as a parameter,
-  by default, it will return the distance in centimeters.
-  To change the default, replace CM by INC.
-  */
+   * If the unit of measure is not passed as a parameter,
+   * by default, it will return the distance in centimeters.
+   * To change the default, replace CM by INC.
+   */
   return distanceRead(CM);
 }
